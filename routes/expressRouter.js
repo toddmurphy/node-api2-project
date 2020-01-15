5f047ebb26b56bacf2552e95730cb876b59a8aae
toddmurphy <toddmurphy93@gmail.com>
2020-01-15 00:40:54 -0400

    delete working

--- routes/expressRouter.js
+++ routes/expressRouter.js
@@ -22,4 +22,46 @@ router.get('/', (req, res) => {
     });
 });
 
+//GET ALL POSTS:id
+router.get('/:id', (req, res) => {
+  // do something with an id
+  const id = req.params.id;
+
+  db.findById(id)
+    .then(post => {
+      if (id) {
+        res.status(200);
+        res.json(post);
+      } else {
+        res.status(404);
+        res.json({ message: 'The post with the specified ID does not exist.', error });
+      }
+    })
+    .catch(error => {
+      res.status(500);
+      res.json({ error: 'The post information could not be retrieved.', error });
+    });
+});
+
+//DELETE A POST
+router.delete('/:id', (req, res) => {
+  //Use ID to find a specific post to delete
+  const deleteID = req.params.id;
+
+  db.remove(deleteID)
+    .then(deletedPost => {
+      if (!deleteID) {
+        res.status(404);
+        res.json({ message: 'The post with the specified ID does not exist.' });
+      } else {
+        res.status(200);
+        res.json({ deletedPost });
+      }
+    })
+    .catch(error => {
+      res.status(500);
+      res.json({ error: 'The post could not be removed', error });
+    });
+});
+
 module.exports = router;
