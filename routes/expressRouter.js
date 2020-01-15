d5e575745b52ef9d3b9d2d1240d9854fecc73f6c
toddmurphy <toddmurphy93@gmail.com>
2020-01-15 00:06:31 -0400

    get posts working

--- routes/expressRouter.js
+++ routes/expressRouter.js
@@ -4,4 +4,22 @@ const db = require('../data/db');
 const router = express.Router();
 router.use(express.json());
 
+// router.get('/', (req, res) => {
+//   res.status(200);
+//   res.send('hello from the GET /api/posts');
+// });
+
+//GET ALL POSTS
+router.get('/', (req, res) => {
+  db.find()
+    .then(posts => {
+      res.status(200);
+      res.json(posts);
+    })
+    .catch(error => {
+      res.status(500);
+      res.json({ error: 'The posts information could not be retrieved.', error });
+    });
+});
+
 module.exports = router;
